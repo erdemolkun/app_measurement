f323f628e6c955055ba596f599cd1f12dafa564c
ERDEM OLKUN <erdem.olkun@turkcell.com.tr>
2019-07-29 13:33:52 +0300

    Add sleep duration to arguments

--- run_apk.sh
+++ run_apk.sh
@@ -1,12 +1,18 @@
 
 echo "------------------ Starting Tests   ---------------"
-echo "\n\nActivity-> $1 | Package-> $2 | Times-> $3\n"
 
 START_ACTIVITY_PATH=$1
 PACKAGE=$2
 REPEAT_COUNT=$3
 
-SLEEP=3
+SLEEP=.6
+
+if [ ! -z "$4" ]; then	
+	SLEEP=$4	
+fi
+
+
+echo "\n\nActivity-> $1 | Package-> $2 | Times-> $3 | Sleep-> $SLEEP seconds\n"
 
 logCatResult=""
 successCount=0
@@ -37,12 +43,13 @@ sleep_now
 for ((i=0;i<$REPEAT_COUNT;i++)); do
     start_app
     logCatResult="$(adb logcat -d -t 2000 | grep 'ActivityManager: Displayed')"
-    echo "LOGCAT RESULT : $logCatResult"
+    echo "LOGCAT RESULT : $logCatResult" # TODO detect if another activity log
 
 	if [ ! -z "$logCatResult" ]; then	
 		#duration=$(echo $logCatResult|cut -d" " -f 18|cut -c2-|cut -d"m" -f1)
 		duration=$(echo ${logCatResult##*:}|cut -d"m" -f1) # 1s200ms TODO parse this
 		echo "Duration : $duration"
+		
 		j=0 
 		parsedMs=0
 		STR_ARRAY=(`echo $duration | tr "s" "\n"`)
@@ -80,8 +87,9 @@ for ((i=0;i<$REPEAT_COUNT;i++)); do
 	sleep_now
 done
 
+echo "Success Ratio :  $successCount/$REPEAT_COUNT"
 echo "Average Boot Duration :  $((totalDuration/successCount)) ms"
-echo "Success Ratio :  $successCount/$REPEAT_COUNT\n"
+
 
 
 
