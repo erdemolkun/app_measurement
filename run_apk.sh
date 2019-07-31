7ca49ffb362a1f7adb9cf5a73ba05270c9ecf35b
ERDEM OLKUN <erdem.olkun@turkcell.com.tr>
2019-07-31 10:42:57 +0300

    Use getops.

--- run_apk.sh
+++ run_apk.sh
@@ -1,3 +1,49 @@
+#!/bin/bash
+usage() { echo "Usage: $0 [-s <com.package.name/activity] [-p <com.package.name>]" 1>&2; exit 1; }
+
+while getopts ":s:p:" o; do
+    case "${o}" in
+        s)
+            s=${OPTARG}
+            #((s == 45 || s == 90)) || usage
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${s}" ] || [ -z "${p}" ]; then
+    usage
+fi
+
+
+echo "s = ${s}"
+echo "p = ${p}"
+
+START_ACTIVITY_PATH=${s}
+PACKAGE=${p}
+REPEAT_COUNT=$1
+
+if [ -z "${1}" ]; then
+    REPEAT_COUNT=1
+fi
+
+# Default sleep value
+SLEEP_DURATION=1
+
+# Assign command parameter to sleep if available
+if [ ! -z "$2" ]; then	
+	SLEEP_DURATION="$2" 	
+fi
+
+echo "REPEAT_COUNT : $REPEAT_COUNT"
+echo "SLEEP_DURATION : $SLEEP_DURATION"
+
 if ! type adb; then
     echo "adb not found"
     echo "check PATH"
@@ -20,23 +66,11 @@ deviceSdkVersion=$(cat /tmp/aa|tr -d $'\r')
 
 echo "Android SDK Version:$deviceSdkVersion"
 
-START_ACTIVITY_PATH=$1
-PACKAGE=$2
-REPEAT_COUNT=$3
 
 MIN_SDK_FOR_MEMORY=21
 MIN_SDK=$(( MIN_SDK_FOR_MEMORY ))
 SDK_VER=$((deviceSdkVersion))
 
-SLEEP=.6
-
-# Assign command parameter to sleep if available
-if [ ! -z "$4" ]; then	
-	SLEEP=$4	
-fi
-
-
-echo "Activity-> $1 | Package-> $2 | Times-> $3 | Sleep-> $SLEEP seconds"
 
 logCatResult=""
 successCount=0
@@ -48,8 +82,8 @@ prevent_sleep() {
 }
 
 sleep_now(){
-	#echo "Sleeping $SLEEP seconds"
-	sleep $SLEEP
+	#echo "Sleeping $SLEEP_DURATION seconds"
+	sleep $((SLEEP_DURATION))
 }
 
 kill_app(){
@@ -82,18 +116,18 @@ for ((i=0;i<$REPEAT_COUNT;i++)); do
 		parsedMs=0
 		STR_ARRAY=(`echo $duration | tr "s" "\n"`)
 		arrayLength=${#STR_ARRAY[@]}
-		for x in "${STR_ARRAY[@]}"
+		for item in "${STR_ARRAY[@]}"
 		do
 
 			if (( $j == 1 )) ; then
-				millis=$(echo $x|cut -d"m" -f1) # extract integer from value with m (ex : 100ms)
+				millis=$(echo $item|cut -d"m" -f1) # extract integer from value with m (ex : 100ms)
 				parsedMs=$((parsedMs+millis))
 
 			elif (( $j == 0 )) ; then
 		    	if(( $arrayLength == 1 )) ; then
 		    		parsedMs=$((duration))
 		    	elif (( $arrayLength == 2 )) ; then
-		    		parsedMs=$((x*1000)) ## Converting seconds to millis
+		    		parsedMs=$((item*1000)) ## Converting seconds to millis
 		    	fi	
 			fi
 
@@ -106,11 +140,11 @@ for ((i=0;i<$REPEAT_COUNT;i++)); do
 		#echo "Parsed Duration : $duration totalDuration : $totalDuration parsedMs : $parsedMs\n"
 		successCount=$((successCount+1))
 		sleep_now
-
+		
 		if [ "$SDK_VER" -gt "$MIN_SDK" ]; then			
-			totalPssMemoryInner="$(adb shell dumpsys meminfo com.turkcell.bip | grep 'TOTAL:'|cut -d':' -f2|cut -d'T' -f1)"
+			totalPssMemoryInner="$(adb shell dumpsys meminfo $PACKAGE | grep 'TOTAL:'|cut -d':' -f2|cut -d'T' -f1)"
     	else
-    		totalPssMemoryInner="$(adb shell dumpsys meminfo com.turkcell.bip | grep 'TOTAL' | tr -s ' '|cut -d ' ' -f 3)"
+    		totalPssMemoryInner="$(adb shell dumpsys meminfo $PACKAGE | grep 'TOTAL' | tr -s ' '|cut -d ' ' -f 3)"
 		fi
     	
     	#adb shell dumpsys meminfo com.turkcell.bip | grep 'TOTAL' | tr -s ' '|cut -d ' ' -f 3 # old devices
@@ -120,19 +154,20 @@ for ((i=0;i<$REPEAT_COUNT;i++)); do
 
 	    
 	#fi	
-    sleep_now
     prevent_sleep
 	kill_app
 	sleep_now
 done
 
-echo "\n-------- RESULTS ---------\n"
-echo "Average Memory Usage :  $((totalPssMemory/successCount))"
-echo "Average Boot Duration :  $((totalDuration/successCount)) ms"
-echo "Success Ratio :  $successCount/$REPEAT_COUNT"
+echo
+echo "-------- RESULTS ---------"
 
-
-
-
-
+if((successCount<=0)); then
+	echo "Insufficent Result Count !!!"
+else
+	echo "Average Memory Usage :  $((totalPssMemory/successCount))"
+	echo "Average Boot Duration :  $((totalDuration/successCount)) ms"
+	echo "Success Ratio :  $successCount/$REPEAT_COUNT"
+	echo
+fi
 
